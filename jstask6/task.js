86e7a2eaa0e88bf88e997515415352a3d51b07e8
muhammad anfar <anfarmuhamma8@gmail.com>
2024-11-21 15:57:59 +0300

    first commit

---
+++ jstask6/task.js
@@ -0,0 +1,15 @@
+// Print sum of marks of all the student
+let student =[
+    {name:"Smith",rollNumber:31,marks:80},
+    {name:"Jenny",rollNumber:15,marks:69},
+    {name:"John",rollNumber:16,marks:35},
+    {name:"Tiger",rollNumber:7,marks:55},
+];
+let marks= [];
+for(i=0;i<student.length;i++){
+    marks.push(student[i].marks);
+}
+console.log(marks);
+
+
+
